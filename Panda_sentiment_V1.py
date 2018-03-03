7e0918b9232acbc1d257f2f865579ac910b1d0d5
Arsenallin <linyang_604@163.com>
2018-03-03 19:43:06 +0800

    Update Panda_sentiment_V1.py

--- Panda_sentiment_V1.py
+++ Panda_sentiment_V1.py
@@ -7,6 +7,8 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 
+# neg.txt：5331条负面电影评论(http://blog.topspeedsnail.com/wp-content/uploads/2016/11/neg.txt)
+# pos.txt：5331条正面电影评论 (http://blog.topspeedsnail.com/wp-content/uploads/2016/11/pos.txt)
 pos_file = 'pos.txt'
 neg_file = 'neg.txt'
 
